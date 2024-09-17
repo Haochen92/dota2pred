cb9d245f7165972c97c3bdcd4573c397fd0f5feb
liuhaochen <liu_hao_chen@hotmail.com>
2024-09-17 03:40:12 +0000

    Modified set_logging.py to fix a bug where error logs are not being printed to the right file path. Also, adjusted time zone to be inline with other files

--- backend/src/utils/set_logging.py
+++ backend/src/utils/set_logging.py
@@ -1,16 +1,21 @@
 import logging
+from src.config import ROOT_DIR
+import pytz
+from datetime import datetime
 
 def get_logger(name):
+ 
+    output_fpath = ROOT_DIR / 'logs' / f'{name}_error.log'
     logger = logging.getLogger(name)
-    
-    # Set logger level to INFO to capture INFO and higher level logs
+    logger.propagate = False
     logger.setLevel(logging.INFO)
     
     # Avoid adding handlers multiple times
     if not logger.hasHandlers():
+        print("No handlers found, setting up handlers.")
         # Set up handlers
         console_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler('error.log')
+        file_handler = logging.FileHandler(output_fpath)
 
         # Configure handlers
         console_handler.setLevel(logging.INFO)
@@ -18,6 +23,7 @@ def get_logger(name):
 
         console_format = logging.Formatter('%(name)s - %(message)s')
         file_format = logging.Formatter('Time: %(asctime)s - File: %(filename)s - Name: %(name)s - Error Msg: %(message)s')
+        file_format.converter = lambda *args: datetime.now(pytz.timezone('Asia/Shanghai')).timetuple()
 
         console_handler.setFormatter(console_format)
         file_handler.setFormatter(file_format)
@@ -25,9 +31,16 @@ def get_logger(name):
         # Add handlers to the logger
         logger.addHandler(console_handler)
         logger.addHandler(file_handler)
-        
+    else:
+        print("Handlers already set up.")
+        for handler in logger.handlers:
+            print("printing handlers")
+            print(f"Handler: {handler}, Level: {handler.level}")
     
     return logger
 
 if __name__ == "__main__":
-    get_logger(__name__)
+    logger = get_logger(__name__)
+    logger.info("testing console log")
+    logger.error("testing error log")
+    
