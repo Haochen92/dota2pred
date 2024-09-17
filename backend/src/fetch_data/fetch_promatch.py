1cf0a68c81cb55e630a12c3b7bcd22d34e7e30df
liuhaochen <liu_hao_chen@hotmail.com>
2024-09-17 07:30:45 +0000

    Adding prefects caching features to cache api calls results

--- backend/src/fetch_data/fetch_promatch.py
+++ backend/src/fetch_data/fetch_promatch.py
@@ -1,14 +1,17 @@
 from prefect import task, flow
+from prefect.cache_policies import INPUTS, TASK_SOURCE
 import pandas as pd
 import time 
 import yaml
 import requests
 from retry import retry
-from datetime import datetime 
+from datetime import datetime, timedelta
 from src.config import ROOT_DIR
-import asyncio
+from src.utils.set_logging import get_logger
 import pytz
 
+# Config logging
+logger = get_logger(__name__)
 
 # Get the current date in YYYYMMDD format
 timezone = pytz.timezone('Asia/Singapore')
@@ -18,6 +21,7 @@ current_date = datetime.now(timezone).strftime('%Y%m%d')
 root_path = ROOT_DIR
 constants_file_path = root_path / 'constants' / 'constants.yml'
 output_file_path = root_path / 'data' / 'pro_match_ids' / f'dota2_pro_match_ids_{current_date}.csv'
+
     
 
 BASE_URL = 'https://api.opendota.com/api/proMatches'
@@ -29,15 +33,17 @@ OUTPUT_COLUMNS = [
     'dire_team_id', 'leagueid', 'league_name', 'start_time', 'radiant_win'
 ]
 
-@task(retries=3, retry_delay_seconds=2)
+@task(retries=3, retry_delay_seconds=2, cache_policy=INPUTS + TASK_SOURCE, cache_expiration=timedelta(days=1))
 def fetch_data(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.json()
     except requests.exceptions.HTTPError as err:
+        logger.error(err)
         raise RuntimeError(f"HTTP error occurred when accessing {url}: {err}")
     except requests.exceptions.RequestException as e:
+        logger.error(err)
         raise RuntimeError(f"An error occurred when accessing {url}: {e}")
 
 @task
@@ -48,6 +54,7 @@ def save_to_csv(data_json, include_header=True):
     selected_json = [{col: x[col] for col in OUTPUT_COLUMNS} for x in data_json]
     output = pd.DataFrame(selected_json)
     output.to_csv(output_file_path, encoding='utf-8', mode='a', index=False, header=include_header)
+
 
 @flow   
 def fetch_promatch_ids(): 
@@ -62,7 +69,7 @@ def fetch_promatch_ids():
 
     while max_match_id > min_match_id:
         search_url = BASE_URL + PARAMETER_STRING + str(max_match_id)
-        print("Requesting:", search_url)
+        logger.info(f"Requesting: {search_url}")
 
         data_json = fetch_data(search_url)
 
@@ -76,7 +83,7 @@ def fetch_promatch_ids():
             first_iteration = False  # Update the flag after capturing the value
         
         max_match_id = min([x['match_id'] for x in data_json])
-        print("Sleeping...")
+        logger.info("Sleeping...")
         time.sleep(SLEEP_DURATION)
     
     if initial_max_match_id:  # Ensure it was captured
