ace348c96cec29aae242b9739051b300312254e2
liuhaochen <liu_hao_chen@hotmail.com>
2025-03-08 12:44:35 +0000

    added Creating HeroIcon component for heros svg files

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroIcon from "@/components/HeroIcon";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <HeroIcon hero_id={1}/>
         <ol>
           <li>
             Get started by editing <code>src/app/page.js</code>.
---
+++ frontend/src/components/HeroIcon.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function HeroIcon ({hero_id}) {
+    return <Image 
+                src={`/icons/heros/${hero_id}.svg`}
+                height={400}
+                width={400}
+                alt={`${hero_id}.svg`}
+            />
+}
