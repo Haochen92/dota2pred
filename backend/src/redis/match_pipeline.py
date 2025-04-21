c5dcbe2ec1166b7cfa33aadf0eed16d7e48f72c1
liuhaochen <liu_hao_chen@hotmail.com>
2025-04-21 14:41:38 +0000

    Completed the entire pipeline with the right Type hints

--- backend/src/redis/match_pipeline.py
+++ backend/src/redis/match_pipeline.py
@@ -4,13 +4,15 @@ import pandas as pd
 from typing import Dict, Any, Set, Optional
 from .redis_client import RedisClient
 from src.utils.set_logging import get_logger
-from src.postgresql import get_engine
-from src.pipeline.datafetching.fetch_live_leagues import retrieve_live_league_games
-from src.pipeline.datafetching.fetch_match_details import get_match_details
-from src.pipeline.preprocessing.preprocessing import preprocess_df
-from src.pipeline.features import create_and_store_hero_features, TeamFeatureProcessor, PlayerHeroFeatures
-from src.pipeline.features.feature_transformation import get_transformed_features
-from src.pipeline.inference.model_inference import get_prediction
+from src.postgresql import get_async_engine
+from data_pipeline.fetching.fetch_live_leagues import fetch_live_league_games
+from data_pipeline.fetching.fetch_match_details import get_match_details
+from ml_pipeline.preprocessing.preprocessing import preprocess_df
+from ml_pipeline.features import create_and_store_hero_features, TeamFeatureProcessor, PlayerHeroFeatures
+from ml_pipeline.features.feature_transformation import get_transformed_features
+from ml_pipeline.inference.model_inference import PredictionService
+from pydantic_models.match import Match
+from data_pipeline.storage.match_repository import insert_pro_matches
 
 logger = get_logger(__name__)
 
@@ -24,16 +26,17 @@ COMPLETION_GROUP = 'completion_group'
 class MatchPipeline:
     """Pipeline for processing live matches, making predictions, and tracking outcomes."""
     
-    def __init__(self):
-        self.redis = RedisClient.get_instance()
-        self.db = get_engine()
+    def __init__(self, env:str = 'prod'):
+        self.env = env
+        self.redis = RedisClient.get_instance(self.env)
+        self.engine = get_async_engine(self.env)
         self._ensure_consumer_groups()
         
     def _ensure_consumer_groups(self) -> None:
         self._create_group(ONGOING_STREAM, PREDICTION_GROUP)
         self._create_group(PREDICTED_STREAM, COMPLETION_GROUP)
         
-    def _create_group(self, stream:str, group: str) -> None:
+    def _create_group(self, stream:str, group:str) -> None:
         try:
             self.redis.xgroup_create(stream, group, id='0', mkstream=True)
             logger.info(f"Created consumer group {group} for stream {stream}")
@@ -48,22 +51,22 @@ class MatchPipeline:
         # Statistics of total matches performed
         try:
             # get current live matches
-            curr_matches = self._get_current_matches()
+            curr_matches = await self._get_current_matches()
             if not curr_matches:
                 logger.info("No live matches found")
                 return 0
             
             # update live matches set and identify new matches
-            new_match_ids = self._update_live_matches_set(curr_matches)
+            new_match_ids =  self._update_live_matches_set(curr_matches)
             
             # process new matches
-            count_new_matches = self._process_new_matches(new_match_ids, curr_matches)
+            count_new_matches = await self._process_new_matches(new_match_ids, curr_matches)
             
             # process ongoing matches
-            count_predicted_matches = self._process_ongoing_matches(curr_matches)
+            count_predicted_matches = await self._process_ongoing_matches(curr_matches)
             
             # process predicted matches for completion
-            count_completed_matches = self._process_predicted_matches(curr_matches)
+            count_completed_matches = await self._process_predicted_matches(curr_matches)
             
             logger.info(
                 f"""Pipeline stats: 
@@ -82,15 +85,15 @@ class MatchPipeline:
             logger.error(f"Unexpected error in poll_live_matches: {str(e)}")
             return 0
         
-    async def _get_current_matches(self) -> Dict[str, Dict[str, Any]]:
+    async def _get_current_matches(self) -> Dict[int, Dict[str, Any]]:
         
-        res = retrieve_live_league_games()
+        res = await fetch_live_league_games()
         if not res:
             return {}
         
-        return {item['match_id']: item for item in res}
+        return {match.match_id: match.model_dump() for match in res }
 
-    async def _update_live_matches_set(self, curr_matches: Dict[str, Dict[str, Any]]) -> Set[str]:
+    def _update_live_matches_set(self, curr_matches: Dict[int, Dict[str, Any]]) -> Set[int]:
         
         curr_match_ids = list(curr_matches.keys())
         tmp_key = f'{MATCH_SET}:temp'
@@ -105,13 +108,13 @@ class MatchPipeline:
             # Replace main set with temp set
             self.redis.rename(tmp_key, MATCH_SET)
             logger.info(f"Found {len(new_matches_ids)} new matches")
-            return new_matches_ids
+            return set(new_matches_ids)
         else:
             return set()
         
     async def _process_new_matches(self, 
-                             new_match_ids: Set[str], 
-                             curr_matches: Dict[str, Dict[str, Any]]
+                             new_match_ids: Set[int], 
+                             curr_matches: Dict[int, Dict[str, Any]]
     ) -> int:
         if not new_match_ids:
             return 0
@@ -130,14 +133,14 @@ class MatchPipeline:
                 )
                 pipe.xadd(
                     ONGOING_STREAM, 
-                    {'match_id': str(match_id), 'timestamp': timestamp}
+                    {'match_id': match_id, 'timestamp': timestamp}
                 )
                 matches_processed += 1
         pipe.execute()
         
         return matches_processed
         
-    async def _process_ongoing_matches(self, curr_matches: Dict[str, Dict[str, Any]]) -> int:
+    async def _process_ongoing_matches(self, curr_matches: Dict[int, Dict[str, Any]]) -> int:
         """Process ongoing matches for predictions.
         
         Args:
@@ -166,14 +169,12 @@ class MatchPipeline:
                 
                 if game_duration > 0:
                     # Feature Engineering
-                    self._create_features(match_details)
+                    await self._create_and_store_features(match_details)
                     
                     # Match Prediction
-                    predicted_outcome = self._get_match_prediction(match_id)
+                    predicted_outcome = await self._get_match_prediction(match_id)
                     
-                    # Append Prediction
-                    
-                    # Update match status
+                    # Append prediction and Update match status
                     pipe.hset(f'match_details:{match_id}',
                               mapping={
                                 'status': 'predicted',
@@ -184,7 +185,7 @@ class MatchPipeline:
                     # Add to predicted matches stream
                     pipe.xadd(
                         PREDICTED_STREAM, 
-                        {'match_id': str(match_id), 'timestamp': timestamp}
+                        {'match_id': match_id, 'timestamp': timestamp}
                     )
                     
                     # Acknowledge processing of this event
@@ -195,20 +196,21 @@ class MatchPipeline:
         pipe.execute()
         return events_processed
     
-    async def _create_and_store_features(self, match_details: Dict[str]):
+    async def _create_and_store_features(self, match_details: Dict[str, Any]):
         input_df = pd.DataFrame([match_details])
         df = preprocess_df(input_df)
-        create_and_store_hero_features(df)
-        TeamFeatureProcessor.create_and_store_team_features(df)
-        PlayerHeroFeatures.create_and_store_player_hero_features(df)
+        await create_and_store_hero_features(df, self.engine)
+        await TeamFeatureProcessor.create_and_store_team_features(df, self.engine)
+        await PlayerHeroFeatures.create_and_store_player_hero_features(df, self.engine)
         
-    async def _get_match_prediction(match_id: str) -> int:
-        df_features = get_transformed_features(match_id)
-        prediction = get_prediction(df_features)
+    async def _get_match_prediction(self, match_id: int) -> int:
+        df_features = await get_transformed_features(engine=self.engine, match_id=match_id)
+        prediction_service = PredictionService(self.engine)
+        prediction = await prediction_service.predict_and_store(df_features)
         return prediction
         
     
-    async def _process_predicted_matches(self, curr_matches: Dict[str, Dict[str, Any]]) -> int:
+    async def _process_predicted_matches(self, curr_matches: Dict[int, Dict[str, Any]]) -> int:
         """Process predicted matches for completion.
         
         Args:
@@ -244,8 +246,8 @@ class MatchPipeline:
                             'status':'completed', 
                             'radiant_win': outcome
                         })
-                        # Here you would store the outcome in your database
-                         # Get the match details
+                        
+                         # Get the match details and Store in db
                         pipe.hgetall(f'match_details:{match_id}')
                         
                         # Execute to get the updated match details
@@ -255,9 +257,7 @@ class MatchPipeline:
                         # Get second outcome
                         match_details = results[1] 
                         
-                        db_operation = await self._store_to_database(match_details)
-                        if not db_operation:
-                            continue
+                        await insert_pro_matches(Match(**match_details))
                         
                         cleanup_pipe = self.redis.pipeline()
                         # For now we're just cleaning up
@@ -271,18 +271,9 @@ class MatchPipeline:
         
         return events_processed
 
-    async def _get_match_outcome(self, match_id: str) -> Optional[bool]:
+    async def _get_match_outcome(self, match_id: int) -> Optional[bool]:
         res = await get_match_details(match_id)
         if not res:
             return None
         
         return res.get('radiant_win', None)
-    
-    async def _store_to_database(self, match_details: Dict) -> bool:
-        '''Store result to database'''
-        try:
-            # store pro_match
-            # store model_prediction
-            return True
-        except Exception as e:
-            return False
