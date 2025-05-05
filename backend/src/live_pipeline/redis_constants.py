c8472d491112a48df6a3e5f14e997669c3181046
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-05 11:16:55 +0000

    delete files from git

--- backend/src/live_pipeline/redis_constants.py
+++
@@ -1,8 +0,0 @@
-MATCH_SET = 'live_match_ids'
-MATCH_STATUS = 'match_status'
-
-ONGOING_STREAM = 'ongoing_matches'
-PREDICTED_STREAM = 'predicted_matches'
-
-PREDICTION_GROUP = 'prediction_group'
-COMPLETION_GROUP = 'completion_group'
