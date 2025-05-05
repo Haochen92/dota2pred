819c311b13e8173a32c86c609972e001b27cdce7
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-05 11:03:22 +0000

    renamed to completion orchestrator, no longer accepts a match_details dictionary as inputs, instead fetches curr match id using redis

---
+++ backend/src/live_pipeline/completion_orchestrator.py
@@ -0,0 +1,85 @@
+from data_extraction.fetch_match_details import fetch_match_details, MatchesAPIResponse
+from pydantic_models.match import MatchesAPIResponse
+from utils.set_logging import get_logger
+from typing import Dict, Any, Set
+from .history_update_service import HistoryUpdateService
+from .redis_service import RedisService
+from data_repository.match_repository import MatchRepository
+from constants.redis_constants import STREAM_PENDING_COMPLETION, COMPLETION_GROUP
+
+logger = get_logger(__name__)
+
+class CompletionOrchestrator:
+    def __init__(
+        self, 
+        redis_service: RedisService,
+        history_update_service: HistoryUpdateService,
+        match_repository: MatchRepository
+    ):
+        self.redis = redis_service
+        self.history_updater = history_update_service
+        self.storage = match_repository
+    
+    async def process_predicted_matches(self, curr_matches: Dict[int, Dict[str, Any]]) -> int:
+        """Process predicted matches for completion.
+        
+        Args:
+            curr_matches: Dictionary of match_id to match details
+        """
+        # Read new events from the predicted matches stream
+        events = await self.redis.fetch_matches_pending_completion('consumer_one')
+        
+        if not events:
+            return 0
+
+        curr_match_set = set(curr_matches.keys())
+        
+        completed_matches = self._filter_completed_matches(events=events, curr_match_set=curr_match_set)
+        
+        events_processed = 0
+        for event_id, data in completed_matches.items():
+            match_id = int(data.get('match_id'))
+            try:
+                completed_match_details: MatchesAPIResponse = await fetch_match_details(match_id)
+                
+                if not completed_match_details:
+                    raise ValueError(f"Match {match_id} is completed but data not available from source")
+                
+                match_outcome = completed_match_details.radiant_win
+                
+                await self.storage.insert_match_outcome(match_id, match_outcome)
+                
+                await self.history_updater.update_histories(completed_match_details)
+                
+                await self.redis.update_completed_matches(match_id, event_id)
+                
+                events_processed += 1
+            except Exception as e:
+                logger.error(f"Failed to process predicted matches for event:{event_id}, match: {match_id}")
+                await self.redis.record_failure_and_ack(
+                    original_stream=STREAM_PENDING_COMPLETION,
+                    group=COMPLETION_GROUP,
+                    event_id=event_id,
+                    event_data=data,
+                    error=e
+                )
+                continue
+        
+        return events_processed
+    
+    async def _filter_completed_matches(self, events: Dict[str, dict], curr_match_set: Set[int]) -> Dict[str, Dict[str, Any]]:
+        
+        completed_matches_dict = {}
+        curr_match_set = await self.redis.get_live_match_ids()
+        
+        for event_id, data in events.items():
+            raw_match_id = data.get('match_id')
+            try:
+                match_id = int(raw_match_id)
+            except (TypeError, ValueError):
+                logger.warning(f"Failed to find or convert match_id, value:{raw_match_id} in event: {event_id}")
+                continue
+            if match_id not in curr_match_set:
+                completed_matches_dict[event_id] = data
+            
+        return completed_matches_dict
