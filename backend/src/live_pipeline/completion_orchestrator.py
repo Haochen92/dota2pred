22ced487195143ab44b8555e947a399736eceb6d
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-08 13:07:23 +0000

    feature: added pydantic models for validating redis input/output

--- backend/src/live_pipeline/completion_orchestrator.py
+++ backend/src/live_pipeline/completion_orchestrator.py
@@ -1,11 +1,13 @@
 from data_extraction.fetch_match_details import fetch_match_details, MatchesAPIResponse
 from pydantic_models.match import MatchesAPIResponse
+from pydantic_models.redis_models import StreamMatchEventData, FailureRecord
 from utils.set_logging import get_logger
 from typing import Dict, Any, Set, Coroutine, List
 from .history_update_service import HistoryUpdateService
 from .redis_service import RedisService
 from data_repository.match_repository import MatchRepository
-from constants.redis_constants import STREAM_PENDING_COMPLETION, COMPLETION_GROUP
+from constants.redis_constants import STREAM_PENDING_COMPLETION
+from utils.time_utils import get_current_utc_iso_timestamp
 from utils.async_utils import get_outcome_concurrently, run_updates_as_group
 
 logger = get_logger(__name__)
@@ -21,28 +23,26 @@ class CompletionOrchestrator:
         self.history_updater = history_update_service
         self.storage = match_repository
     
-    async def process_predicted_matches(self, curr_matches: Dict[int, Dict[str, Any]]) -> int:
+    async def run_completion_cycle(self) -> int:
         """Process predicted matches for completion.
         
         Args:
             curr_matches: Dictionary of match_id to match details
         """
         # Read new events from the predicted matches stream
-        events = await self.redis.fetch_matches_pending_completion('consumer_one')
+        
+        events: Dict[str, StreamMatchEventData] = await self.redis.fetch_matches_pending_completion('consumer_one')
         
         if not events:
             return 0
-
-        curr_match_set = set(curr_matches.keys())
         
         # Filter those match that were previously from event stream but is no longer live
-        completed_matches = await self._filter_completed_matches(events=events, curr_match_set=curr_match_set)
+        completed_matches: Dict[str, StreamMatchEventData] = await self._filter_completed_matches(events=events)
         
         fetch_match_details_task_group: Dict[int, Coroutine[Any, Any, MatchesAPIResponse]] = {}
         
         for event_id , data in completed_matches.items():
-            match_id = data.get('match_id')
-            fetch_match_details_task_group[event_id] = fetch_match_details(match_id)
+            fetch_match_details_task_group[event_id] = fetch_match_details(data.match_id)
         
         # Fetch API responses
         outcome_dict: Dict[str, MatchesAPIResponse| Exception ] = await get_outcome_concurrently(
@@ -50,7 +50,6 @@ class CompletionOrchestrator:
         )
         
         events_processed = 0
-        
         for event_id, match_model in outcome_dict.items():
             try:
                 if match_model is None:
@@ -75,31 +74,29 @@ class CompletionOrchestrator:
                 events_processed += 1
             except Exception as e:
                 original_data = completed_matches.get(event_id, None)
-                logger.error(f"Failed to process predicted matches for event:{event_id}, data: {original_data} ")
-                await self.redis.record_failure_and_ack(
+                data_dict = original_data.model_dump()
+                logger.error(f"Failed to process predicted matches for event:{event_id}, data: {data_dict}")
+                
+                failure_record = FailureRecord(
+                    original_data=data_dict,
+                    original_event_id=event_id,
                     original_stream=STREAM_PENDING_COMPLETION,
-                    group=COMPLETION_GROUP,
-                    event_id=event_id,
-                    event_data=original_data,
-                    error=e
+                    error_message=e,
+                    failure_timestamp=get_current_utc_iso_timestamp()
                 )
+                await self.redis.record_failure_and_ack(failure_record.model_dump())
                 continue
             
 
         return events_processed
     
-    async def _filter_completed_matches(self, events: Dict[str, dict], curr_match_set: Set[int]) -> Dict[str, Dict[str, Any]]:
+    async def _filter_completed_matches(self, events: Dict[str, StreamMatchEventData]) -> Dict[str, StreamMatchEventData]:
         
         completed_matches_dict = {}
-        curr_match_set = await self.redis.get_live_match_ids()
+        curr_match_set: Set[int] = await self.redis.get_live_match_ids()
         
         for event_id, data in events.items():
-            raw_match_id = data.get('match_id')
-            try:
-                match_id = int(raw_match_id)
-            except (TypeError, ValueError):
-                logger.warning(f"Failed to find or convert match_id, value:{raw_match_id} in event: {event_id}")
-                continue
+            match_id = data.match_id
             if match_id not in curr_match_set:
                 completed_matches_dict[event_id] = data
             
