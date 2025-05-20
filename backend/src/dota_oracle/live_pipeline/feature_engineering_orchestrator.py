41a58215a2bd41a29fa6ace3b047c862b7041db2
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-20 10:03:44 +0000

    refactor: refactored imports for poetry and fixed type hints

--- backend/src/dota_oracle/live_pipeline/feature_engineering_orchestrator.py
+++ backend/src/dota_oracle/live_pipeline/feature_engineering_orchestrator.py
@@ -1,13 +1,14 @@
 import pandas as pd
-from typing import Dict, Coroutine, Any
-from utils.set_logging import get_logger
+from typing import Dict, Coroutine, Any, List
+from dota_oracle.utils.set_logging import get_logger
 from .feature_engineering_service import FeatureEngineeringService
 from .redis_service import RedisService
-from constants.redis_constants import STREAM_NEW_MATCHES, FEATURE_ENGINEER_GROUP
-from data_repository.match_repository import MatchRepository
-from utils.async_utils import run_updates_as_group, get_outcome_concurrently
-from utils.time_utils import get_current_utc_iso_timestamp
-from pydantic_models.redis_models import StreamMatchEventData, FailureRecord
+from dota_oracle.constants.redis_constants import STREAM_NEW_MATCHES, FEATURE_ENGINEER_GROUP
+from dota_oracle.data_repository.match_repository import MatchRepository
+from dota_oracle.data_repository.schemas import MatchTable
+from dota_oracle.utils.async_utils import run_updates_as_group, get_outcome_concurrently
+from dota_oracle.utils.time_utils import get_current_utc_iso_timestamp
+from dota_oracle.pydantic_models.redis_models import StreamMatchEventData, FailureRecord
 
 logger = get_logger(__name__)
 
@@ -48,29 +49,46 @@ class FeatureEngineeringOrchestrator:
         else:
             logger.info(f"FeatureEngineeringOrchestrator: Found {len(events)} new events from {STREAM_NEW_MATCHES}")
         
-        match_details_dict: Dict[int, Dict[str, Any]] = await self._fetch_matches_from_db(events)
+        match_details_dict: Dict[int, MatchTable] = await self._fetch_matches_from_db(events)
         
         if not match_details_dict:
             logger.warning(f"no match details found in the database")
             return 0
         
         events_task_dict : Dict[str, Coroutine[Any, Any, Any]] = {}
-        for event_id, data in events.items():
-            match_id = data.match_id
-            match_details = match_details_dict.get(match_id)
-            input_dataframe = pd.DataFrame([match_details])
-            
-            events_task_dict[event_id] = self._engineer_and_update_redis(
-                input_dataframe, 
-                match_id,
-                event_id,
-                data
-            )
-        
-        output_dict = await get_outcome_concurrently(events_task_dict)
         
         successful_events = 0
         failed_events = 0
+        
+        for event_id, data in events.items():
+            try:
+                match_id = data.match_id
+                match_details_instance = match_details_dict.get(match_id)
+                if not match_details_instance:
+                    raise ValueError(f"match details for {match_id} is empty")
+                
+                events_task_dict[event_id] = self._engineer_and_update_redis(
+                    match_details_instance, 
+                    match_id,
+                    event_id,
+                    data
+                )
+            except Exception as e:
+                failure_record = FailureRecord(
+                    original_event_id=event_id,
+                    original_stream=STREAM_NEW_MATCHES,
+                    original_group=FEATURE_ENGINEER_GROUP,
+                    original_data=data,
+                    error_type=type(e).__name__,
+                    error_message=str(e),
+                    failure_timestamp=get_current_utc_iso_timestamp()
+                )
+                await self.redis.record_failure_and_ack(failure_record)
+                failed_events += 1
+        
+        output_dict = await get_outcome_concurrently(events_task_dict)
+        
+        
         
         for event, outcome in output_dict.items():
             if outcome and not isinstance(outcome, Exception):
@@ -81,9 +99,9 @@ class FeatureEngineeringOrchestrator:
         logger.info(f"Feature engineering orchestrator orchestrated {successful_events} successful events, and {failed_events} failed events")
         return successful_events
     
-    async def _engineer_and_update_redis(self, input_dataframe: pd.DataFrame, match_id: int, event_id: str, data: StreamMatchEventData) -> bool:
+    async def _engineer_and_update_redis(self, match_details: MatchTable, match_id: int, event_id: str, data: StreamMatchEventData) -> bool:
         task_group = [
-            self.feature_engineering_service.create_and_store_features(input_dataframe),
+            self.feature_engineering_service.create_and_store_features(match_details),
             self.redis.advance_match_to_pending_prediction(match_id, event_id)
         ]
         try:
@@ -95,7 +113,8 @@ class FeatureEngineeringOrchestrator:
                 original_stream=STREAM_NEW_MATCHES,
                 original_group=FEATURE_ENGINEER_GROUP,
                 original_data=data,
-                error_message=e,
+                error_type=type(e).__name__,
+                error_message=str(e),
                 failure_timestamp=get_current_utc_iso_timestamp()
             )
             await self.redis.record_failure_and_ack(failure_record)
@@ -103,12 +122,12 @@ class FeatureEngineeringOrchestrator:
         
     
     
-    async def _fetch_matches_from_db(self, events: Dict[str, StreamMatchEventData]) -> Dict[int, Dict[str, any]]:
+    async def _fetch_matches_from_db(self, events: Dict[str, StreamMatchEventData]) -> Dict[int, MatchTable]:
         list_matches_instances = [data.match_id for _, data in events.items()]
         batch_matches = await self.storage.get_match_details_batch(list_matches_instances)
         
         if not batch_matches:
             return {}
-        match_dict = {match.match_id : match.model_dump() for match in batch_matches}
-        return match_dict
         
+        matches_dict = {match_instance.match_id : match_instance for match_instance in batch_matches}
+        return matches_dict
