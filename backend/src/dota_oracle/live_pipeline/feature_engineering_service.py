a1e359c03496605a8c3d16c7206dde047a92f565
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-20 04:39:44 +0000

    refactor: change feature creation using sqlmodels instead of pandas df

--- backend/src/dota_oracle/live_pipeline/feature_engineering_service.py
+++ backend/src/dota_oracle/live_pipeline/feature_engineering_service.py
@@ -1,46 +1,48 @@
-from feature_engineering import TeamFeatureProcessor, PlayerHeroFeaturesProcessor, create_hero_features, preprocess_live_match_data
-from data_repository.features_repository import FeaturesRepository
-import pandas as pd
-from utils.set_logging import get_logger
-from utils.async_utils import run_updates_as_group
+from dota_oracle.feature_engineering import TeamFeatureProcessor, PlayerHeroFeaturesProcessor, create_hero_features, preprocess_live_match_data
+from dota_oracle.data_repository.features_repository import FeaturesRepository
+from dota_oracle.data_repository.schemas import MatchTable, HeroFeaturesTable, TeamFeaturesTable, PlayerHeroFeatureTable
+from dota_oracle.utils.set_logging import get_logger
+from dota_oracle.utils.async_utils import run_updates_as_group
 from typing import Coroutine, List, Any
 
 logger = get_logger(__name__)
                         
 class FeatureEngineeringService:
+    
     def __init__(
         self, 
-        team_feature_processor = TeamFeatureProcessor,
-        player_hero_processor = PlayerHeroFeaturesProcessor,
-        features_repository = FeaturesRepository,
+        team_feature_processor: TeamFeatureProcessor,
+        player_hero_processor: PlayerHeroFeaturesProcessor,
+        features_repository: FeaturesRepository,
     ):
         self.team_feature_processor = team_feature_processor
         self.player_hero_processor = player_hero_processor
         self.storage = features_repository
         
-    async def create_and_store_features(self, match_dataframe: pd.DataFrame) -> None:
-        if match_dataframe.empty:
-            raise ValueError("input Dataframe is empty")
+    async def create_and_store_features(self, match_instance: MatchTable) -> None:
+        if not match_instance:
+            logger.warning("no match_instances for data creation")
+            return None
         
-        processed_dataframe = preprocess_live_match_data(match_dataframe)
-        if processed_dataframe.empty:
+        processed_match_instance = preprocess_live_match_data(match_instance)
+        if not processed_match_instance:
             raise ValueError("Dataframe is empty after processing")
         
         try:
-            hero_features: pd.DataFrame = create_hero_features(processed_dataframe)
-            team_features: pd.DataFrame = await self.team_feature_processor.create_team_features(processed_dataframe)
-            player_hero_features: pd.DataFrame = await self.player_hero_processor.create_player_hero_features(processed_dataframe)
+            hero_features: List[HeroFeaturesTable] = create_hero_features([processed_match_instance])
+            team_features: List[TeamFeaturesTable] = await self.team_feature_processor.create_team_features([processed_match_instance]) 
+            player_hero_features: List[PlayerHeroFeatureTable] = await self.player_hero_processor.create_player_hero_features([processed_match_instance]) 
         except Exception as e:
             logger.error(f"Error creating features {e}", exc_info=True)
             raise e
         
-        if hero_features.empty or team_features.empty or player_hero_features.empty:
-            raise ValueError("features dataframes are created but empty")
+        if not hero_features or not team_features or not player_hero_features:
+            raise ValueError("Features are not created successfully")
         
-        storage_task: List[Coroutine[Any, Any, None]] = [
-            self.storage.store_features(hero_features),
-            self.storage.store_features(team_features),
-            self.storage.store_features(player_hero_features)
+        storage_task: list[Coroutine[Any, Any, None]] = [
+            self.storage.store_features(feature_instances=hero_features, table_class=HeroFeaturesTable),
+            self.storage.store_features(feature_instances=team_features, table_class=TeamFeaturesTable), 
+            self.storage.store_features(feature_instances=player_hero_features, table_class=PlayerHeroFeatureTable) 
         ]
         
         try:
