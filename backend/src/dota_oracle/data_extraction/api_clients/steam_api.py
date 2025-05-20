a41fff6938d4f9b8196dcee8cc49433483b92d27
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-20 13:13:12 +0000

    refactor: add check to ensure API_kEY is not none before proceeding

--- backend/src/dota_oracle/data_extraction/api_clients/steam_api.py
+++ backend/src/dota_oracle/data_extraction/api_clients/steam_api.py
@@ -1,18 +1,22 @@
 import os
 import aiohttp
-from typing import Optional
+from typing import Optional, Dict
 from dotenv import load_dotenv
 
 load_dotenv()
 
 STEAM_URL = 'http://api.steampowered.com/'
-API_KEY = os.getenv('STEAM_API')
+API_KEY: str | None = os.getenv('STEAM_API')
+
 
 
 async def fetch_steam_data(endpoint: str) -> Optional[dict]:
+    if API_KEY is None:
+        raise ValueError("STEAM_API environment variable is not set. Cannot make API calls.")
+    
     url = f'{STEAM_URL}{endpoint}'
     async with aiohttp.ClientSession() as session:
-        params = {'key':API_KEY}
+        params:Dict[str, str] = {'key': API_KEY}
         async with session.get(url, params=params) as response:
             if response.status != 200:
                 raise ValueError(f"Request failed with status {response.status}, retrying...")
