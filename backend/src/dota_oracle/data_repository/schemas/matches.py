8bbaa8910728338720c8b0374794abc6a5e60db1
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-20 00:33:28 +0000

    refactor: add optional[str] for hero_id

--- backend/src/dota_oracle/data_repository/schemas/matches.py
+++ backend/src/dota_oracle/data_repository/schemas/matches.py
@@ -9,16 +9,16 @@ class MatchTable(SQLModel, table=True):
     match_id: int = Field(default=None, sa_column=Column('match_id', BigInteger, primary_key=True))
     
     # Columns for hero IDs:
-    slot_0_hero_id: Optional[int] = Field(default=None) 
-    slot_1_hero_id: Optional[int] = Field(default=None)
-    slot_2_hero_id: Optional[int] = Field(default=None)
-    slot_3_hero_id: Optional[int] = Field(default=None)
-    slot_4_hero_id: Optional[int] = Field(default=None)
-    slot_128_hero_id: Optional[int] = Field(default=None)
-    slot_129_hero_id: Optional[int] = Field(default=None)
-    slot_130_hero_id: Optional[int] = Field(default=None)
-    slot_131_hero_id: Optional[int] = Field(default=None)
-    slot_132_hero_id: Optional[int] = Field(default=None)
+    slot_0_hero_id: Optional[str] = None
+    slot_1_hero_id: Optional[str] = None 
+    slot_2_hero_id: Optional[str] = None 
+    slot_3_hero_id: Optional[str] = None 
+    slot_4_hero_id: Optional[str] = None 
+    slot_128_hero_id: Optional[str] = None 
+    slot_129_hero_id: Optional[str] = None 
+    slot_130_hero_id: Optional[str] = None 
+    slot_131_hero_id: Optional[str] = None 
+    slot_132_hero_id: Optional[str] = None 
     
     # Columns for account IDs:
     slot_0_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
