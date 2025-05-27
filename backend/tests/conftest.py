2786ebe9ffa0ce443c60e1a5b7a3d5168c417695
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-27 04:56:25 +0000

    feature: create database fixtures

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,15 +2,27 @@ import pytest
 import pytest_asyncio
 import asyncio
 
+# Postgresql Imports
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
+from dota_oracle.data_repository import schemas
+
+# Redis Imports
 from testcontainers.redis import RedisContainer
+from testcontainers.postgres import PostgresContainer
 import redis.asyncio as aioredis
 
-from dota_oracle.utils.set_logging import get_logger
-
+# Services import
 from dota_oracle.live_pipeline.redis_service import RedisService
 
 
+from dota_oracle.utils.set_logging import get_logger
+
+
+
+
 logger = get_logger(__name__)
+
 
 @pytest.fixture(scope='session')
 def redis_container_instance():
@@ -53,3 +65,46 @@ async def redis_service_test_subject(redis_container_instance: RedisContainer):
     yield service
     # Clean up the client used by the service
     await service_redis_client.aclose()
+    
+
+@pytest.fixture(scope='session')
+def postgres_container_instance():
+    with PostgresContainer("postgres:14-alpine") as postgres_c:
+        logger.info(f"Redis container started on {postgres_c.get_container_host_ip()}:{postgres_c.get_exposed_port(5432)}")
+        yield postgres_c
+        logger.info("Redis Container Stopped")
+        
+
+@pytest_asyncio.fixture(scope="session")
+async def test_postgres_engine(postgres_container_instance: PostgresContainer):
+    
+    # construct url directly for async engine
+    host = postgres_container_instance.get_container_host_ip()
+    port = postgres_container_instance.get_exposed_port(5432)
+    user = postgres_container_instance.username
+    password = postgres_container_instance.password
+    dbname = postgres_container_instance.dbname
+
+    async_db_url = f"postgresql+asyncpg://{user}:{password}@{host}:{port}/{dbname}"
+    engine = create_async_engine(async_db_url)
+    logger.info(f"Test DB engine created for: {async_db_url}")
+    yield engine
+    logger.info("Disposing Test DB engine.")
+    await engine.dispose()
+
+@pytest_asyncio.fixture(scope="session", autouse=True)
+async def create_db_tables(test_postgres_engine: AsyncEngine):
+    """
+    Connects to the test DB and creates all tables defined in SQLModel.metadata.
+    Runs once per session.
+    """
+    app_metadata_to_create = SQLModel.metadata
+    
+    async with test_postgres_engine.begin() as conn:
+        logger.info("Dropping existing tables for a clean state")
+        await conn.run_sync(SQLModel.metadata.drop_all)
+        logger.info("Creating all tables in test database")
+        await conn.run_sync(app_metadata_to_create.create_all)
+        
+    logger.info("Database tables created in test PostgreSQL container.")
+        
