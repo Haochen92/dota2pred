3934395d3a6a7d41ce2fe34cdcd1fd7ef6d06b20
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-28 12:26:14 +0000

    refactor: change MatchTable's field columns to mandatory for some fields. Add cascade for dependent table of matchtable

--- backend/src/dota_oracle/data_repository/schemas/matches.py
+++ backend/src/dota_oracle/data_repository/schemas/matches.py
@@ -12,37 +12,37 @@ class MatchTable(SQLModel, table=True):
     leagueid: Optional[int] = None
     
     # Columns for hero IDs:
-    slot_0_hero_id: Optional[str] = None
-    slot_1_hero_id: Optional[str] = None 
-    slot_2_hero_id: Optional[str] = None 
-    slot_3_hero_id: Optional[str] = None 
-    slot_4_hero_id: Optional[str] = None 
-    slot_128_hero_id: Optional[str] = None 
-    slot_129_hero_id: Optional[str] = None 
-    slot_130_hero_id: Optional[str] = None 
-    slot_131_hero_id: Optional[str] = None 
-    slot_132_hero_id: Optional[str] = None 
+    slot_0_hero_id: str
+    slot_1_hero_id: str 
+    slot_2_hero_id: str 
+    slot_3_hero_id: str 
+    slot_4_hero_id: str 
+    slot_128_hero_id: str 
+    slot_129_hero_id: str 
+    slot_130_hero_id: str 
+    slot_131_hero_id: str 
+    slot_132_hero_id: str 
     
     # Columns for account IDs:
-    slot_0_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_1_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_2_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_3_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_4_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_128_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_129_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_130_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_131_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
-    slot_132_account_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
+    slot_0_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_1_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_2_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_3_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_4_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_128_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_129_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_130_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_131_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
+    slot_132_account_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
     
     radiant_name: Optional[str] = Field(default=None)
-    radiant_team_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
+    radiant_team_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
     dire_name: Optional[str] = Field(default=None)
-    dire_team_id: Optional[int] = Field(default=None, sa_column=Column(BigInteger, nullable=True)) 
+    dire_team_id: int = Field(default=None, sa_column=Column(BigInteger, nullable=False)) 
     
-    start_time: Optional[datetime] = Field(
+    start_time: datetime = Field(
         default=None,
-        sa_column=Column(TIMESTAMP(timezone=True), nullable=True)
+        sa_column=Column(TIMESTAMP(timezone=True), nullable=False)
     ) 
     duration: Optional[float] = Field(default=None)
 
@@ -53,4 +53,4 @@ class MatchOutcomeTable(SQLModel, table=True):
         sa_column=Column(BigInteger, primary_key=True) 
     )
     
-    radiant_win: Optional[bool] = Field(default=None) 
+    radiant_win: bool = Field(default=None) 
