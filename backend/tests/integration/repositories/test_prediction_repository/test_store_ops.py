41a4b474c2c6ee8fdbe82d5f0b5fa7e91ca62777
liuhaochen <liu_hao_chen@hotmail.com>
2025-05-30 09:07:12 +0000

    chore: remove unused imports

--- backend/tests/integration/repositories/test_prediction_repository/test_store_ops.py
+++ backend/tests/integration/repositories/test_prediction_repository/test_store_ops.py
@@ -1,16 +1,12 @@
 import pytest
-import pytest_asyncio
 
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
-from sqlalchemy.dialects.postgresql import insert
-from sqlmodel import delete, select
+from sqlmodel import select
 
 from ....factories.repository_factories import MatchPredictionTableFactory
 from dota_oracle.data_repository.schemas import MatchPredictionTable
 from dota_oracle.data_repository.prediction_repository import PredictionRepository
 
-
-from typing import List, Tuple, Any, AsyncGenerator, Dict, Set
 
 from dota_oracle.utils.set_logging import get_logger
 
