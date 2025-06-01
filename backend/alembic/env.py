107e6269e8d7076ffc34cb5f883408e83ff9c06e
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-01 01:47:50 +0000

    chore: update imports

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,6 @@
 from logging.config import fileConfig
 from sqlmodel import SQLModel
-from dota_oracle.data_repository import schemas
+from dota_oracle import models
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
