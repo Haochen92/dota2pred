8bd41438df648808c31878dfce4690c3162d1249
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-03 13:11:14 +0000

    update imports

--- backend/tests/integration/repositories/test_heroes_repository/conftest.py
+++ backend/tests/integration/repositories/test_heroes_repository/conftest.py
@@ -1,14 +1,10 @@
 import pytest
 import pytest_asyncio
 
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
-from sqlmodel import delete, select
-
-from sqlalchemy.dialects.postgresql import insert as pginsert
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from dota_oracle.data_repository.heroes_repository import HeroesRepository
 from ....factories.repository_factories import HeroDataTableFactory
-from dota_oracle.data_repository.schemas import HeroDataTable
 from dota_oracle.utils import get_logger 
 
 from typing import Dict
@@ -18,24 +14,8 @@ logger = get_logger(__name__)
 pytestmark = pytest.mark.asyncio(loop_scope="session")
 
 @pytest_asyncio.fixture(scope='function')
-async def hero_repository_test_subject(test_postgres_engine: AsyncEngine) -> HeroesRepository:
-    return HeroesRepository(engine=test_postgres_engine)
-
-@pytest_asyncio.fixture(scope='function', autouse=True)
-async def clear_heroes_database(test_postgres_engine):
-    async with AsyncSession(test_postgres_engine) as session:
-        async with session.begin():
-            await session.execute(delete(HeroDataTable))
-
-    logger.info(f"hero_database set up complete")
-    
-    yield
-    
-    async with AsyncSession(test_postgres_engine) as session:
-        async with session.begin():
-            await session.execute(delete(HeroDataTable))
-    
-    logger.info("test ended, successfully cleared database records")
+async def hero_repository_test_subject(db_session: AsyncSession) -> HeroesRepository:
+    return HeroesRepository(session=db_session)
     
     
 @pytest_asyncio.fixture(scope='function')
