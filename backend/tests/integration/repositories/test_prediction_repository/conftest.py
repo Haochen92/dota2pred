620ba9dc46c7cb07249bd46660e3c5e6e3ba1273
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-04 09:28:29 +0000

    remove setup and teardown of tables. replace engine fixture with session fixture

--- backend/tests/integration/repositories/test_prediction_repository/conftest.py
+++ backend/tests/integration/repositories/test_prediction_repository/conftest.py
@@ -1,11 +1,11 @@
 import pytest
 import pytest_asyncio
 
-from sqlalchemy.ext.asyncio import AsyncSession, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import delete
 
 from ....factories.repository_factories import MatchPredictionTableFactory
-from dota_oracle.data_repository.schemas import MatchPredictionTable
+from dota_oracle.models.inference import MatchPredictionTable
 from dota_oracle.data_repository.prediction_repository import PredictionRepository
 
 from dota_oracle.utils.set_logging import get_logger
@@ -18,30 +18,12 @@ pytestmark = pytest.mark.asyncio(loop_scope='session')
 
 
 @pytest_asyncio.fixture(scope='function')
-async def prediction_repository_test_subject(test_postgres_engine: AsyncEngine):
-    return PredictionRepository(test_postgres_engine)
+async def prediction_repository_test_subject(db_session: AsyncSession):
+    return PredictionRepository(db_session)
 
-
-@pytest_asyncio.fixture(scope='function', autouse=True)
-async def setup_and_clear_table(test_postgres_engine):
-    
-    async with AsyncSession(test_postgres_engine) as session:
-        async with session.begin():
-            await session.execute(delete(MatchPredictionTable))
-            
-    logger.info(f"cleared database, test setup complete")
-    
-    yield
-    
-    async with AsyncSession(test_postgres_engine) as session:
-        async with session.begin():
-            await session.execute(delete(MatchPredictionTable))
-            
-    logger.info(f"test completed, database cleared successfully") 
-    
     
 @pytest_asyncio.fixture(scope='function')
-async def seed_prediction_data(test_postgres_engine) -> List[MatchPredictionTable]:
+async def seed_prediction_data(db_session) -> List[MatchPredictionTable]:
     prediction_data = [
         MatchPredictionTableFactory.build(match_id=1001, predictor_name='random_forest'),
         MatchPredictionTableFactory.build(match_id=1001, predictor_name='xg_boost'), # Same match, different model
@@ -49,14 +31,9 @@ async def seed_prediction_data(test_postgres_engine) -> List[MatchPredictionTabl
         MatchPredictionTableFactory.build(match_id=1003, predictor_name='random_forest'),
     ]
     
-    async with AsyncSession(test_postgres_engine, expire_on_commit=False) as session:
-        async with session.begin():
-            session.add_all(prediction_data)
+    db_session.add_all(prediction_data)
             
-        
-            
-        session.expunge_all() # Detach the instances
-            
+    await db_session.flush()
     logger.info(f"prediction data seeding completed - inserted {len(prediction_data)} records")
     
     return prediction_data
