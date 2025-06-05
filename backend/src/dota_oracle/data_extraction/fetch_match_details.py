1955073fb76126eb521faef2a51255d6184b8c14
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-05 09:21:39 +0000

    refactor: delete logger handler set up

--- backend/src/dota_oracle/data_extraction/fetch_match_details.py
+++ backend/src/dota_oracle/data_extraction/fetch_match_details.py
@@ -1,18 +1,12 @@
 from .api_clients.opendota_api import fetch_opendota_api
 from dota_oracle.models.match import MatchesAPIResponse
-from dota_oracle.config import ROOT_DIR
 from dota_oracle.utils.set_logging import get_logger
 from typing import Optional
-import logging
+
 from pydantic import ValidationError
 
 # Set up logger
 logger = get_logger(__name__)
-match_info_handler = logging.FileHandler(f'{ROOT_DIR}/logs/stored_pro_matchs.log')
-handler_format = logging.Formatter('%(asctime)s - %(message)s')
-match_info_handler.setFormatter(handler_format)
-match_info_handler.addFilter(lambda record: record.levelno == logging.INFO)
-logger.addHandler(match_info_handler)
 
 async def fetch_match_details(match_id: int) -> Optional[MatchesAPIResponse]:
     endpoint = f'matches/{match_id}'
