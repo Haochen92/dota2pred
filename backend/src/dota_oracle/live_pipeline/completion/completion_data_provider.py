fa93410af650d74ab31648233ef9e7d084c1adee
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-07 04:57:11 +0000

    feature: create class for providing event data

---
+++ backend/src/dota_oracle/live_pipeline/completion/completion_data_provider.py
@@ -0,0 +1,49 @@
+from dota_oracle.utils.set_logging import get_logger
+from typing import List
+from ..redis_service import RedisService
+from .fetch_outcome_service import FetchOutcomeService
+from dota_oracle.models.pipeline import CompletionWorkItem
+from dota_oracle.constants.redis_constants import STREAM_PENDING_COMPLETION
+
+logger = get_logger(__name__)
+
+class CompletionDataProvider:
+    def __init__(
+        self,
+        redis_service: RedisService,
+    ):
+        self.redis = redis_service
+     
+    async def get_work_items(self, consumer_name: str = "default_consumer") -> List[CompletionWorkItem]:
+        
+        # Fetch all pending events
+        events = await self.redis.fetch_matches_pending_completion(consumer_name)
+        
+        if not events: 
+            logger.info(f"No events in {STREAM_PENDING_COMPLETION}")
+            return []
+        
+        # For all matches in the pending list, attempt to retrieve their outcome by batch
+        pending_id_list = [data.match_id for _, data in events.items()]
+        outcome_map = await FetchOutcomeService.fetch_outcomes_batch(pending_id_list)
+        
+        if not outcome_map:
+            logger.info("None of the currently predicted live matches have completed")
+            return []
+        
+        work_item_list: List[CompletionWorkItem] = []
+        # Filter and return completed matches
+        for event_id, event_data in events.items():
+            match_id = event_data.match_id
+            outcome = outcome_map.get(match_id)
+            
+            if outcome:
+                work_item = CompletionWorkItem(
+                    event_id = event_id,
+                    event_data = event_data,
+                    outcome = outcome
+                )
+                
+                work_item_list.append(work_item)
+        
+        return work_item_list
