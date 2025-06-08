b1536f5e6caf72d5ae4e7306b302d170aeaf2edf
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-08 11:37:08 +0000

    register feature using polyfactory plugin

--- backend/tests/factories/redis_models_factory.py
+++ backend/tests/factories/redis_models_factory.py
@@ -3,13 +3,17 @@ from dota_oracle.models.redis.schema import (
     StreamMatchEventData, MatchStatusValue,
     FailureRecord
 )
+from polyfactory.pytest_plugin import register_fixture
 
+@register_fixture
 class StreamMatchEventDataFactory(ModelFactory[StreamMatchEventData]):
     pass
 
+@register_fixture
 class MatchStatusValueFactory(ModelFactory[MatchStatusValue]):
     pass
 
+@register_fixture
 class FailureRecordFactory(ModelFactory[FailureRecord]):
     pass
 
