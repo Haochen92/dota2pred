0dd355f58438247bb28167576009575bcb1d5972
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-10 07:51:32 +0000

    add detailed docstrings with attributes description

--- backend/src/dota_oracle/models/redis/schema.py
+++ backend/src/dota_oracle/models/redis/schema.py
@@ -8,6 +8,12 @@ class MatchProcessingStatus(str, Enum):
     """
     Represents the possible statuses of a match during processing.
     The string value is what's stored in Redis.
+    
+    # Attributes
+    New = "new"
+    PENDING_PREDICTION = "pending_prediction"
+    PENDING_COMPLETION = "pending_completion"
+    
     """
     NEW = "new"
     PENDING_PREDICTION = "pending_prediction"
@@ -19,7 +25,10 @@ class MatchProcessingStatus(str, Enum):
 class StreamMatchEventData(BaseModel):
     """
     Data model for messages in the match processing streams.
-    e.g., {'match_id': '12345', 'timestamp': isoformat}
+    
+    Attributes:
+        match_id: int -> unique identifier for a match
+        timestamp: datetime -> datetime object which will be serialised by "serialize_timestamp" method to isoformat
     """
     match_id: int 
     timestamp: datetime
@@ -42,6 +51,15 @@ class FailureRecord(BaseModel):
     """
     Data model for the information stored in DLQ hashes when an event fails.
     This is serialized to JSON before being stored in the hash.
+    
+    Attributes:
+        original_group: str (Group from Incoming Stream)
+        original_event_id: str (Event_id from Incoming Stream)
+        original_stream: str (Incoming Stream)
+        original_data: StreamMatchEventData (Event data from Incoming Stream)
+        error_type: str 
+        error_message: str
+        failure_timestamp: datetime 
     """
     original_group: str
     original_event_id: str
