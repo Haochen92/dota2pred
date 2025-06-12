b7139b19ab415c7246b1aaeefc3e0f118e39bf3c
Haochen <lhcliuhaochen@gmail.com>
2025-06-12 11:21:22 +0800

    modify code to use the new ongoing_league_game pydantic model as DTO

--- backend/src/dota_oracle/live_pipeline/data_fetching/new_match_data_provider.py
+++ backend/src/dota_oracle/live_pipeline/data_fetching/new_match_data_provider.py
@@ -1,9 +1,10 @@
 from typing import List, Set
 from dota_oracle.utils.set_logging import get_logger
-from dota_oracle.models.live_games.schema import LiveLeagueGame
+from dota_oracle.models.live_games.schema import LiveLeagueGame, OngoingLeagueGame
 from dota_oracle.data_extraction.fetch_live_leagues import fetch_live_league_games
 from ..services.redis_service import RedisService
 from dota_oracle.models.pipeline import NewMatchWorkItem
+from pydantic import ValidationError
 
 logger = get_logger(__name__)
 
@@ -21,15 +22,15 @@ class NewMatchDataProvider:
             List of NewMatchWorkItem for processing
         """
         # Fetch current live matches
-        curr_matches = await self._fetch_current_matches()
-        if not curr_matches:
+        ongoing_matches = await self._fetch_ongoing_matches()
+        if not ongoing_matches:
             logger.info("No live matches found")
             return []
         
-        logger.info(f"Found {len(curr_matches)} live matches")
+        logger.info(f"Found {len(ongoing_matches)} live matches")
         
         # Filter for new matches
-        new_matches = await self._filter_new_matches(curr_matches)
+        new_matches = await self._filter_new_matches(ongoing_matches)
         if not new_matches:
             logger.info("No new matches found")
             return []
@@ -47,16 +48,30 @@ class NewMatchDataProvider:
         
         return work_items
     
-    async def _fetch_current_matches(self) -> List[LiveLeagueGame]:
-        """Fetches current live league games from API."""
+    async def _fetch_ongoing_matches(self) -> List[OngoingLeagueGame]:
+        """Fetches current live league games from API & extract ongoing matches"""
         try:
             curr_games = await fetch_live_league_games()
-            return curr_games or []
+            ongoing_games = await self._extract_ongoing_matches(curr_games)
+            return ongoing_games
         except Exception as e:
             logger.warning(f"Error fetching matches from API: {e}", exc_info=True)
             raise
+        
+    async def _extract_ongoing_matches(self, all_curr_games: List[LiveLeagueGame]) -> List[OngoingLeagueGame]:
+        ongoing_games_list: List[OngoingLeagueGame] = []
+        for game in all_curr_games:
+            try:
+                ongoing_game = OngoingLeagueGame.model_validate(game)
+                ongoing_games_list.append(ongoing_game)
+            except ValidationError as ve:
+                logger.info(f"match {game.match_id} is live but game has not started")
+                continue
+            except Exception as e:
+                raise
+        return ongoing_games_list
     
-    async def _filter_new_matches(self, curr_matches: List[LiveLeagueGame]) -> List[LiveLeagueGame]:
+    async def _filter_new_matches(self, curr_matches: List[OngoingLeagueGame]) -> List[OngoingLeagueGame]:
         """Filters current matches to return only new ones."""
         curr_match_ids = [match.match_id for match in curr_matches]
         new_match_set: Set[int] = await self.redis.update_live_match_set_and_get_new(curr_match_ids)
