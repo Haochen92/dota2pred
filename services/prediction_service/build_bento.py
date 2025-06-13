449b381939506d421efabc935f9513bdc7491339
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-13 14:05:51 +0000

    add scripts

---
+++ services/prediction_service/build_bento.py
@@ -0,0 +1,13 @@
+from ...backend.bentoml.build_service import main
+import subprocess
+
+def docker_compose_up():
+    subprocess.run(
+        ['docker', 'compose', 'up', '-d', '--force-recreate', '--no-deps', 'bentoml'],
+        check=True  # This will raise an exception if the command fails
+    )
+    
+if __name__ == "__main__":
+    main()
+    docker_compose_up()
+
