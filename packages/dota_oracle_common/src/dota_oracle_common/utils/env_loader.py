eaa0e1701957b28bdb661b6c5b6e918fd4f76f0d
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-14 01:30:44 +0000

    add helper to load environmental file

---
+++ packages/dota_oracle_common/src/dota_oracle_common/utils/env_loader.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv, find_dotenv
+from dota_oracle_common.utils import get_logger
+
+logger = get_logger(__name__)
+
+def load_workspace_env():
+    env_file = find_dotenv('.env')
+    if env_file:
+        load_dotenv(env_file)
+        return True
+    else:
+        logger.warning("unable to find environment file")
+        return False
+
+
+# Auto load when imported
+load_workspace_env()
