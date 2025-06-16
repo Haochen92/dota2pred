9e755801e4aaa7b92d6e65490539bfe00bcbb144
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-16 11:43:55 +0000

    add bentoml fixture for integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@ pytest_plugins = [
     "tests.fixtures.database",
     "tests.fixtures.redis", 
     "tests.fixtures.repositories",
+    "tests.fixtures.bentoml",
     "tests.fixtures.pipeline_services",
     "tests.fixtures.data_providers",
     "tests.fixtures.event_processors",
@@ -9,5 +10,5 @@ pytest_plugins = [
     "tests.fixtures.feature_engineering",
     "tests.factories.repository_factories",
     "tests.factories.unit_test_factory",
-    "tests.factories.redis_models_factory"
+    "tests.factories.redis_models_factory",
 ]
