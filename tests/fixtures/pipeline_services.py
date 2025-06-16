2085b50de612badc56d8cc2e4fee14a29b73efe4
liuhaochen <liu_hao_chen@hotmail.com>
2025-06-16 11:45:22 +0000

    pass bentoml_service_container fixture for initialising model_inference_service

--- tests/fixtures/pipeline_services.py
+++ tests/fixtures/pipeline_services.py
@@ -63,6 +63,6 @@ def feature_preparation_service(mock_model_inference_service, model_meta_data_ap
 
 
 @pytest.fixture
-async def model_inference_service() -> ModelInferenceService:
-    service = await ModelInferenceService.create()
+async def model_inference_service(prediction_server_container) -> ModelInferenceService:
+    service = await ModelInferenceService.create(base_url=prediction_server_container)
     return service
