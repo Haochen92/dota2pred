633973c47f9fe1795190a94ba0a21e751b5b3c9b
liuhaochen <liu_hao_chen@hotmail.com>
2025-07-05 09:58:58 +0000

    fix linting issues

--- live_orchestrator_app/src/live_orchestrator_app/prefect/fetch_and_store_promatches.py
+++ live_orchestrator_app/src/live_orchestrator_app/prefect/fetch_and_store_promatches.py
@@ -1,5 +1,4 @@
 from dota_oracle_pipeline.data_extraction.fetch_pro_match import fetch_pro_match
-from dota_oracle_pipeline.data_extraction.fetch_match_details import fetch_match_details
 from dota_oracle_pipeline.data_extraction.fetch_constants import fetch_constants
 from prefect import flow
 import asyncio
