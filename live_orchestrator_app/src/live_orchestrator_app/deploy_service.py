f27bd4acd6c9b5fe7e772125fe5187ddf4f9c99b
liu haochen <liu_hao_chen@hotmail.com>
2025-07-26 01:52:17 +0000

    add entrypoint to deployment

--- live_orchestrator_app/src/live_orchestrator_app/deploy_service.py
+++ live_orchestrator_app/src/live_orchestrator_app/deploy_service.py
@@ -1,11 +1,15 @@
-from .app_container import start_application
+from live_orchestrator_app.app_container import start_application
 from dota_oracle_common.utils.set_logging import get_logger
 
 logger = get_logger(__name__)
 
 if __name__ == "__main__":
     start_application.deploy(
-        name="live_orchestration_app", work_pool_name="dota-work-pool", cron="*/2 * * * *"  # every 2 minutes
+        name="live_orchestration_app",
+        work_pool_name="dota-work-pool",
+        cron="*/2 * * * *",
+        entrypoint="live_orchestrator_app/app_container.py:start_application",
+        # every 2 minutes
     )
 
     logger.info(msg="Prefect Deployment successful for dota-app")
