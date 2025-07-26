c522445999ae28e3ea740ee0de4df2043b8cfc73
liu haochen <liu_hao_chen@hotmail.com>
2025-07-26 01:51:59 +0000

    remove wait and sleep timer. Run worker process as main process

--- live_orchestrator_app/entrypoint.sh
+++ live_orchestrator_app/entrypoint.sh
@@ -3,23 +3,18 @@ set -e  # Exit immediately if a command exits with a non-zero status
 
 # Ensure the Prefect work pool exists
 echo "Checking for existing work pool..."
-if ! prefect work-pool inspect "work-pool"; then
+if ! prefect work-pool inspect "dota-work-pool"; then
     echo "Work pool does not exist. Creating it now..."
-    prefect work-pool create "work-pool" --type process || echo "Failed to create work-pool"
+    prefect work-pool create "dota-work-pool" --type process || echo "Failed to create work-pool"
 else
     echo "Work pool already exists."
 fi
 
-# Optional: Add a small delay to ensure work pool is created before continuing
-sleep 20  # Adjust the sleep time if necessary
+# Start deployment script
+echo "Starting service..."
+python -m live_orchestrator_app.deploy_service
+
 
 # Start the Prefect worker
-echo "Starting the Prefect worker for the work pool 'work-pool'..."
-prefect worker start -p "work-pool" &  # Run the worker in the background
-
-# Run the Python deployment script
-echo "Starting service..."
-python deploy_service.py
-
-# Wait for background processes to finish (if any)
-wait
+echo "Starting the Prefect worker for the work pool 'dota-work-pool'..."
+exec prefect worker start -p "dota-work-pool"  # Run the worker in the background
