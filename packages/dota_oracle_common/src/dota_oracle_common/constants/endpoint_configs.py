13bd09df1b5e12a006e5ec2c4d0ee475703cb02a
liu haochen <liu_hao_chen@hotmail.com>
2025-08-26 05:01:26 +0000

    changed dynamic endpoint_urls to pydantic property to generate during runtime instead of build time

--- packages/dota_oracle_common/src/dota_oracle_common/constants/endpoint_configs.py
+++ packages/dota_oracle_common/src/dota_oracle_common/constants/endpoint_configs.py
@@ -1,17 +1,30 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Service_URL(BaseSettings):
-    # model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
     BASE_INFERENCE_URL: str = "http://localhost:3333"
-    PUBLIC_MATCHES_INFERENCE_URL: str = f"{BASE_INFERENCE_URL}/predict/public"
-    PUBLIC_MATCHES_METADATA_URL: str = f"{BASE_INFERENCE_URL}/metadata/public"
-    PRO_MATCHES_INFERENCE_URL: str = f"{BASE_INFERENCE_URL}/predict/pro"
-    PRO_MATCHES_METADATA_URL: str = f"{BASE_INFERENCE_URL}/metadata/pro"
-
-    # Frontend Url
     BASE_FRONTEND_URL: str = ""  # Placeholder for frontend URL later
-    PUBLIC_MATCHES_FRONTEND_URL: str = "f{BASE_FRONTEND_URL}/public_matches"
+
+    @property
+    def PUBLIC_MATCHES_INFERENCE_URL(self) -> str:
+        return f"{self.BASE_INFERENCE_URL}/predict/public"
+
+    @property
+    def PUBLIC_MATCHES_METADATA_URL(self) -> str:
+        return f"{self.BASE_INFERENCE_URL}/metadata/public"
+
+    @property
+    def PRO_MATCHES_INFERENCE_URL(self) -> str:
+        return f"{self.BASE_INFERENCE_URL}/predict/pro"
+
+    @property
+    def PRO_MATCHES_METADATA_URL(self) -> str:
+        return f"{self.BASE_INFERENCE_URL}/metadata/pro"
+
+    @property
+    def PUBLIC_MATCHES_FRONTEND_URL(self) -> str:
+        return f"{self.BASE_FRONTEND_URL}/public_matches"
 
 
 service_url = Service_URL()
