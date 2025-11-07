527cdbb130e55c3559c3f2b1ec5557c675e134e4
liu haochen <liu_hao_chen@hotmail.com>
2025-11-07 09:48:32 +0000

    refactor: rename batch feature generator classes to Batch* for clarity

--- packages/dota_oracle_pipeline/src/dota_oracle_pipeline/feature_engineering/batch/__init__.py
+++ packages/dota_oracle_pipeline/src/dota_oracle_pipeline/feature_engineering/batch/__init__.py
@@ -1,24 +1,24 @@
 from .team_features.window import TeamWindowFeatureGenerator
-from .team_features.decay import TeamDecayFeatureGenerator
+from .team_features.decay import BatchTeamDecayFeatureGenerator
 from .hero_wr_features.window import HeroWinrateWindowFeatureGenerator
-from .hero_wr_features.decay import HeroWinrateDecayFeatureGenerator
+from .hero_wr_features.decay import BatchHeroWinrateDecayFeatureGenerator
 from .hero_wr_features import analyze_hero_counts as analyze_hero_pick_counts
 from .player_hero_features import (
     PlayerHeroWindowFeatureGenerator,
-    PlayerHeroDecayFeatureGenerator,
-    PlayerHeroDynamicPriorFeatureGenerator,
+    BatchPlayerHeroDecayFeatureGenerator,
+    BatchPlayerHeroDynamicPriorFeatureGenerator,
     analyze_pair_counts as analyze_player_hero_pair_counts,
 )
 
 
 __all__ = [
     "TeamWindowFeatureGenerator",
-    "TeamDecayFeatureGenerator",
+    "BatchTeamDecayFeatureGenerator",
     "HeroWinrateWindowFeatureGenerator",
-    "HeroWinrateDecayFeatureGenerator",
+    "BatchHeroWinrateDecayFeatureGenerator",
     "analyze_hero_pick_counts",
     "PlayerHeroWindowFeatureGenerator",
-    "PlayerHeroDecayFeatureGenerator",
-    "PlayerHeroDynamicPriorFeatureGenerator",
+    "BatchPlayerHeroDecayFeatureGenerator",
+    "BatchPlayerHeroDynamicPriorFeatureGenerator",
     "analyze_player_hero_pair_counts",
 ]
