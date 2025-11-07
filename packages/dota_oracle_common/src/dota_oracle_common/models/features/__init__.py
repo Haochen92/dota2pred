f8c6edf5b80f54b7e6df97da6432475d01316768
liu haochen <liu_hao_chen@hotmail.com>
2025-11-07 09:46:21 +0000

    feat: export feature schema types and hyperparams from features package

--- packages/dota_oracle_common/src/dota_oracle_common/models/features/__init__.py
+++ packages/dota_oracle_common/src/dota_oracle_common/models/features/__init__.py
@@ -1,9 +1,29 @@
 from .table import TeamFeaturesTable, PlayerHeroFeatureTable, HeroFeaturesTable
-from .schema import AllFeaturesDTO
+from .schema import (
+    TeamFeatures,
+    HeroFeatures,
+    PlayerHeroFeature,
+    AllFeaturesDTO,
+    AggregatedHeroFeatures,
+    AggregatedPlayerHeroFeatures,
+    AggregatedTeamFeatures,
+    TeamFeaturesHyperparams,
+    HeroFeaturesHyperparams,
+    PlayerHeroFeaturesHyperparams,
+)
 
 __all__ = [
     "TeamFeaturesTable",
     "PlayerHeroFeatureTable",
     "HeroFeaturesTable",
+    "TeamFeatures",
+    "HeroFeatures",
+    "PlayerHeroFeature",
     "AllFeaturesDTO",
+    "AggregatedHeroFeatures",
+    "AggregatedPlayerHeroFeatures",
+    "AggregatedTeamFeatures",
+    "TeamFeaturesHyperparams",
+    "HeroFeaturesHyperparams",
+    "PlayerHeroFeaturesHyperparams",
 ]
