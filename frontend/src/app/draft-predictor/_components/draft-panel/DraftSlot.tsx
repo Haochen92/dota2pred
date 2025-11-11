b57bb7d5caa3e744df82020737aec8e3a576e908
liu haochen <liu_hao_chen@hotmail.com>
2025-11-11 09:32:51 +0000

    chore: remove unnecessary key prop on HeroIcon in DraftSlot

--- frontend/src/app/draft-predictor/_components/draft-panel/DraftSlot.tsx
+++ frontend/src/app/draft-predictor/_components/draft-panel/DraftSlot.tsx
@@ -21,7 +21,6 @@ export default function DraftSlot({
                 backgroundColor: heroId ? 'transparent' : theme.colors.gray[9],
             })}
         >{ heroId && <HeroIcon
-            key={heroId}
             hero_id={heroId}
             hero_name='hero_name_placeholder'
             />
